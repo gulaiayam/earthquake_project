a4ea40f472b557dce2d9a7ca0f91c9104ce0f20b
gulaiayam <muhammad.hazmi.abdullah@gmail.com>
2026-01-13 01:35:13 +0700

    Finisih NLP part

--- UAS/functions.php
+++ UAS/functions.php
@@ -1,11 +1,28 @@
 <?php
 include "conn.php";
 
+$production = array();
+$sql_criterias = array(
+                        "semua"=>1,
+                        "gunung"=>"nama_gunung",
+                        "intensitas"=>"intensitas_abu",
+                        "area"=>"area_abu",
+                        "ketinggian"=>"ketinggian_meter"
+                        );
 
-
-function exe($text){
+function exe($text) {
+    global $production;
 
     $words = array("/tampilkan/","/berapa/","/di mana/","/kapan/","/apa/");
+    $tokens = array(
+                    "/semua/",
+                    "/gunung/",
+                    "/intensitas/",
+                    "/area/",
+                    "/ketinggian/",
+                );
+
+    
 
     $result = explode(" ", $text);
 
@@ -34,6 +51,26 @@ function exe($text){
             if($parsing == 1) break;
             $parsing = preg_match($words[$j], $result[$i]);
         }
+
+        for($j = 0; $j < count($tokens); $j++){
+            if($token == 1){
+                if($result[$i] != "semua"){
+                    if(preg_match('/^\\d+$/',$result[$i])){    
+                    $production += [$result[$i] => $result[$i+1]];
+                    }
+
+                    if($result[$i+1] == "abu"){
+                        $production += [$result[$i] => $result[$i+2]];
+                    }else{
+                        $production += [$result[$i] => $result[$i+1]];
+                    }
+                }else if($result[$i] == "semua"){
+                    $production += [$result[$i] => 1];
+                }
+                break;
+            } 
+            $token = preg_match($tokens[$j], $result[$i]);
+        }
         
 
 
@@ -48,7 +85,7 @@ function exe($text){
                 $result[$i]
             </td>
             <td>
-                
+                $token
             </td>
             <td>
                 $parsing
@@ -57,6 +94,120 @@ function exe($text){
     ";
     }
 
-    echo"</table> <br><br>";
+    echo "</table> <br><br>";
+    //var_dump($result);
+    //echo "<br><br>";
+    //var_dump($production);
+    //echo "<br><br>";
+    return $production;
+}
+
+function show_table($array = array()){
+    global $con,$sql_criterias;
+    $flag = false;
+    $i = 0;
+
+        if(sizeof($array)==1){
+             $query = mysqli_query($con, "select * from sigmet_data");
+             $flag = !$flag;
+        }
+        else{
+            $criteria = "where";
+
+            foreach($array as $x => $x_value){
+                if(preg_match("/intensitas/",$x) || preg_match("/area/",$x)){
+                    $criteria .= " " . $sql_criterias[$x]. " like '%" . $x_value . "%'";
+                }else{
+                    $criteria .= " " . $sql_criterias[$x]. " = '" . $x_value. "'";
+                }
+                
+                if($i+1 != count($array)){
+                    $criteria .= " and";
+                }
+                $i++;
+            }
+            $query = mysqli_query($con, "select * from sigmet_data $criteria");
+            $flag = !$flag;
+        }
+            //echo "select * from sigmet_data $criteria";
+            
     
+
+    if($flag){
+        echo "
+        <table>
+            <td>
+                Index
+            </td>
+            <td>
+                Transmisi WIB
+            </td>
+            <td>
+                Valid Mulai WIB
+            </td>
+            <td>
+                Valid Akhir WIB
+            </td>
+            <td>
+                Area Penerbangan
+            </td>
+            <td>
+                Nama Gunung
+            </td>
+            <td>
+                Posisi Gunung
+            </td>
+            <td>
+                Area Abu
+            </td>
+            <td>
+                Ketinggian
+            </td>
+            <td>
+                Pergerakan Abu
+            </td>
+            <td>
+                Intentsitas Abu
+            </td>
+            ";
+    while ($row = mysqli_fetch_array($query)) {
+                        echo "
+                            <tr/>
+                                <td>
+                                    $row[id]
+                                </td>
+                                <td>
+                                    $row[transmisi_wib]
+                                </td>
+                                <td>
+                                    $row[valid_mulai_wib]
+                                </td>
+                                <td>
+                                    $row[valid_akhir_wib]
+                                </td>
+                                <td>
+                                    $row[area_penerbangan]
+                                </td>
+                                <td>
+                                    $row[nama_gunung]
+                                </td>
+                                <td>
+                                    $row[posisi_gunung]
+                                </td>
+                                <td>
+                                    $row[area_abu]
+                                </td>
+                                <td>
+                                    $row[ketinggian_meter] m
+                                </td>
+                                <td>
+                                    $row[pergerakan_abu]
+                                </td>
+                                <td>
+                                    $row[intensitas_abu]
+                                </td>
+                            </tr>
+                        ";
+            }
+    }    
 }
--- UAS/index.php
+++ UAS/index.php
@@ -4,7 +4,7 @@ include "functions.php";
         if(isset($_POST['exe'])){
             $value = $_POST["area"];
         }
-
+        
 ?>
 
 <!DOCTYPE html>
@@ -16,8 +16,10 @@ include "functions.php";
 </head>
 <body>
 
+    <h2>Input Commands</h2>
+
     <form action="" method="post">
-        <textarea name="area" id=""><?php echo $value;?></textarea>
+        <textarea name="area" rows="15" cols="100" id=""><?php echo $value;?></textarea>
         <br>
         <input type="submit" name ="exe" value="Execute">
         <input type="submit" name ="refresh" value="Reset">
@@ -26,17 +28,17 @@ include "functions.php";
 
     <?php
         if(isset($_POST['exe'])){
-            exe($_POST['area']);
+            show_table(exe($_POST['area']));
         }
-        //if(isset($_POST['exe'])){
-        //    show_table();
-        //}
         if(isset($_POST['refresh'])){
             header("Refresh:0");
         }
     ?>
+
 </body>
 </html>
+
+
 
 <style>
     table, th, tr,td {
@@ -52,3 +54,4 @@ include "functions.php";
         font-size: 20px;
     }
 </style>
+
